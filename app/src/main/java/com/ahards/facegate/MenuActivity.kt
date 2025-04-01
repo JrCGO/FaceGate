6115dfb8ab83d4bc076c61b26f074a687e6615f4
Jr <juniosgoulart@gmail.com>
2025-04-01 20:20:24 -0300

    Finalização: menuactivity.xml

--- app/src/main/java/com/ahards/facegate/MenuActivity.kt
+++ app/src/main/java/com/ahards/facegate/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import android.view.View
 import android.widget.LinearLayout
 
@@ -11,10 +12,10 @@ import android.widget.LinearLayout
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menuactivity)
+        setContentView(R.layout.menu_layout2)
 
         // Achando o botão voltar | Retornar para MainActivity
-        val menuArrow: ImageView = findViewById(R.id.arrowmenu)
+        val menuArrow: ImageView = findViewById(R.id.btn_menu_arrow)
         menuArrow.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent) // Iniciando a nova activity
@@ -23,7 +24,7 @@ class MenuActivity : AppCompatActivity() {
         }
 
         // Achando a botão: Lista de Acessos | Ir para ListAcess
-        val AcessList: View = findViewById(R.id.view3)
+        val AcessList: CardView = findViewById(R.id.menu_btn_listAcess)
         AcessList.setOnClickListener {
             val intent = Intent(this, ListAcess::class.java)
             startActivity(intent)
